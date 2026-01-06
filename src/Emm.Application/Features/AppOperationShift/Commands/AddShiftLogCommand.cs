3271378d9784943ecedc214c9f2b9804b37beb3d
Leonardo da Jarviis <jarviisha.dev@gmail.com>
2026-01-06 19:31:21 +0700

    zz

--- src/Emm.Application/Features/AppOperationShift/Commands/AddShiftLogCommand.cs
+++ src/Emm.Application/Features/AppOperationShift/Commands/AddShiftLogCommand.cs
@@ -65,7 +65,6 @@ public sealed record ShiftLogItemRequest
 {
     public Guid? Id { get; init; }
     public Guid ItemId { get; init; }
-    public string ItemName { get; init; } = null!;
     public decimal Quantity { get; init; }
     public Guid AssetId { get; init; }
 }
